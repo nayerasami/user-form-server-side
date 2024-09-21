8aab50bbc92930a60f9671d7e45a658713263de2
nayera sami <nayerasamy19@gmail.com>
2024-09-21 16:14:12 +0300

    add validation functions

--- middlewares/validations/user.validation.js
+++ middlewares/validations/user.validation.js
@@ -1,44 +1,43 @@
-const joi =require('joi')
+const joi = require('joi')
 
 
 const gender = ['Male', 'Female'];
-const maritalStatus =["Single", "Divorced", "Married", "Widower"]
+const maritalStatus = ["Single", "Divorced", "Married", "Widower"]
 
 
 module.exports.validateAddUser = joi.object({
 
-      firstNameAR:joi.string().required().min(2).pattern(/^[\u0600-\u06FF\s]+$/u).trim(),
-      lastNameAR:joi.string().required().min(2).pattern(/^[\u0600-\u06FF\s]+$/u).trim(),
-      firstNameEN:joi.string().required().min(2).pattern(/^[a-zA-Z\s]+$/i).trim(),
-      lastNameEN:joi.string().required().min(2).pattern(/^[a-zA-Z\s]+$/i).trim(),
-      email:joi.string().email().required().pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i).trim(),
-      phoneKey: joi.number().integer().positive().required(),
-      phoneNumber:joi.string().required().min(2).pattern(/^[0-9]+$/i).trim(),
-      nationalID:joi.string().required().min(2).trim(),
-      birthDate:joi.date().required().custom((value,helpers)=>{
-        const currentDate =new Date();
-        if(value >= currentDate){
+    firstNameAR: joi.string().required().min(2).pattern(/^[\u0600-\u06FF\s]+$/u).trim(),
+    lastNameAR: joi.string().required().min(2).pattern(/^[\u0600-\u06FF\s]+$/u).trim(),
+    firstNameEN: joi.string().required().min(2).pattern(/^[a-zA-Z\s]+$/i).trim(),
+    lastNameEN: joi.string().required().min(2).pattern(/^[a-zA-Z\s]+$/i).trim(),
+    email: joi.string().email().required().pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i).trim(),
+    phoneKey: joi.number().integer().positive().required(),
+    phoneNumber: joi.string().required().min(2).pattern(/^[0-9]+$/i).trim(),
+    nationalID: joi.string().required().min(2).trim(),
+    birthDate: joi.date().required().custom((value, helpers) => {
+        const currentDate = new Date();
+        if (value >= currentDate) {
             return helpers.message("Birth date must be in the past")
         }
         return value;
-      }),
-      addressAr:joi.string().required().min(8).max(150).pattern(/^[\u0600-\u06FF\s]+$/u).trim(),
-      addressEN:joi.string().required().min(8).max(150).pattern(/^[a-zA-Z\s]+$/i).trim(),
-      gender: joi.string().valid(...gender).required().min(2).trim(),
-      maritalStatus: joi.string().valid(...maritalStatus).required().min(2).trim(),
-      userExperience: joi.array().items(
+    }),
+    addressAr: joi.string().required().min(8).max(150).pattern(/^[\u0600-\u06FF\s]+$/u).trim(),
+    addressEN: joi.string().required().min(8).max(150).pattern(/^[a-zA-Z\s]+$/i).trim(),
+    gender: joi.string().valid(...gender).required().min(2).trim(),
+    maritalStatus: joi.string().valid(...maritalStatus).required().min(2).trim(),
+    userExperience: joi.array().items(
         joi.object({
             companyName: joi.string().required().min(2).trim(),
             startDate: joi.date().required(),
             endDate: joi.date().optional(),
             currentlyWorking: joi.boolean().optional(),
-          
+
         })
     ).required(),
     permissions: joi.array().items(
         joi.object({
-            id: joi.number().integer().positive().required(),
-            permission: joi.string().required().min(2).trim()
+            permissionId: joi.number().integer().positive().required(),
         })
     ).required()
 
@@ -47,29 +46,43 @@ module.exports.validateAddUser = joi.object({
 
 
 
-module.exports.validateEditUser =joi.object({
+module.exports.validateEditUser = joi.object({
 
     id: joi.number().integer().positive().required(),
-    firstNameAR:joi.string().optional().min(2).pattern(/^[\u0600-\u06FF\s]+$/u).trim(),
-    lastNameAR:joi.string().optional().min(2).pattern(/^[\u0600-\u06FF\s]+$/u).trim(),
-    firstNameEN:joi.string().optional().min(2).pattern(/^[a-zA-Z\s]+$/i).trim(),
-    lastNameEN:joi.string().optional().min(2).pattern(/^[a-zA-Z\s]+$/i).trim(),
-    email:joi.string().email().optional().pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i).trim(),
+    firstNameAR: joi.string().optional().min(2).pattern(/^[\u0600-\u06FF\s]+$/u).trim(),
+    lastNameAR: joi.string().optional().min(2).pattern(/^[\u0600-\u06FF\s]+$/u).trim(),
+    firstNameEN: joi.string().optional().min(2).pattern(/^[a-zA-Z\s]+$/i).trim(),
+    lastNameEN: joi.string().optional().min(2).pattern(/^[a-zA-Z\s]+$/i).trim(),
+    email: joi.string().email().optional().pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i).trim(),
     phoneKey: joi.number().integer().positive().optional(),
-    phoneNumber:joi.string().optional().min(2).pattern(/^[0-9]+$/i).trim(),
-    nationalID:joi.string().optional().min(2).trim(),
-    birthDate:joi.date().optional().custom((value,helpers)=>{
-      const currentDate =new Date();
-      if(value >= currentDate){
-          return helpers.message("Birth date must be in the past")
-      }
-      return value;
+    phoneNumber: joi.string().optional().min(2).pattern(/^[0-9]+$/i).trim(),
+    nationalID: joi.string().optional().min(2).trim(),
+    birthDate: joi.date().optional().custom((value, helpers) => {
+        const currentDate = new Date();
+        if (value >= currentDate) {
+            return helpers.message("Birth date must be in the past")
+        }
+        return value;
     }),
-    addressAr:joi.string().optional().min(8).max(150).pattern(/^[\u0600-\u06FF\s]+$/u).trim(),
-    addressEN:joi.string().optional().min(8).max(150).pattern(/^[a-zA-Z\s]+$/i).trim(),
+    addressAr: joi.string().optional().min(8).max(150).pattern(/^[\u0600-\u06FF\s]+$/u).trim(),
+    addressEN: joi.string().optional().min(8).max(150).pattern(/^[a-zA-Z\s]+$/i).trim(),
     gender: joi.string().valid(...gender).optional().min(2).trim(),
-    maritalStatus: joi.string().valid(...maritalStatus).optional().min(2).trim()
+    maritalStatus: joi.string().valid(...maritalStatus).optional().min(2).trim(),
+    userExperience: joi.array().items(
+        joi.object({
+            companyName: joi.string().required().min(2).trim(),
+            startDate: joi.date().required(),
+            endDate: joi.date().optional(),
+            currentlyWorking: joi.boolean().optional(),
 
-    
+        })
+    ).optional(),
+    permissions: joi.array().items(
+        joi.object({
+            permissionId: joi.number().integer().positive().required(),
+        })
+    ).optional()
+
+
 })
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const userRouter = express.Router();
 const asyncHandler = require("express-async-handler");
- const { getAllUsers, createUser, getOneUser, updateUser, deleteUser, CheckEmail, checkPhone, checkNationalID } = require("../controllers/user.controller");
- const {validation }=require('../middlewares/validations/validation');
+const { getAllUsers, createUser, getOneUser, updateUser, deleteUser, CheckEmail, checkPhone, checkNationalID } = require("../controllers/user.controller");
+const {validation }=require('../middlewares/validations/validation');
 const { validateAddUser, validateEditUser } = require("../middlewares/validations/user.validation");
 
 
@@ -13,11 +13,11 @@ userRouter.route('/check-national-id').get(asyncHandler(checkNationalID))
 
 userRouter.route("/")
 .get(asyncHandler(getAllUsers))
-.post(asyncHandler(createUser));
+.post(validation(validateAddUser),asyncHandler(createUser));
 
 userRouter.route("/:id")
 .get(asyncHandler(getOneUser))
-.put(asyncHandler(updateUser))
+.put(validation(validateEditUser),asyncHandler(updateUser))
 .delete(asyncHandler(deleteUser));
 
 
